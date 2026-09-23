6730002a64b58605e3569dada20ba8fe87222db5
panelmarket <nisayildirim020@gmail.com>
2026-09-23 14:29:34 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import crypto from "crypto";
 import { createClient } from "@supabase/supabase-js";
-import nodemailer from "nodemailer";
+import { google } from "googleapis";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
